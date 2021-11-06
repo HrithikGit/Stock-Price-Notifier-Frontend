f2e74c1061bd7085816d149dfc9714298ab1ca97
HrithikGit <hrithik@mindler.com>
2021-11-06 20:30:22 +0530

    updated files

---
+++ scripts/index.js
@@ -0,0 +1,127 @@
+const url = "http://127.0.0.1:5000/";
+
+const phoneNumberPattern =  /^\d{10}$/;
+
+function getStarted(){
+    document.getElementById("mainContent").textContent = "";
+    document.getElementById("loginDialog").classList.remove("display-none");
+}
+
+function showLoginForm(){
+    document.getElementById("loginForm").classList.remove('display-none');
+    document.getElementById("registerForm").classList.add('display-none');
+}
+
+function showRegisterForm(){
+    document.getElementById("registerForm").classList.remove('display-none');
+    document.getElementById("loginForm").classList.add('display-none');
+}
+
+function loginUser(){
+    var phoneNumber = document.getElementById("loginPhoneNumber").value.trim();
+    var password = document.getElementById("loginPassword").value.trim();
+
+    if(! phoneNumber.match(phoneNumberPattern)){
+        addValidationError("Invalid phone number");
+        return;
+    }
+
+    if( password.length < 6){
+        addValidationError("Invalid password");
+        return;
+    }
+
+    if(phoneNumber.length>0 && password.length>0){
+        var data = {
+            phone : phoneNumber,
+            password : password
+        }
+        post(url+"login",data).then(function(result){
+            console.log(result)
+            if(result.status==="success"){
+                localStorage.setItem('token', result.token);
+                localStorage.setItem('userName', result.name);
+                window.location.href = "stocks.html";
+            }else{
+                alert("Invalid Credentials");
+            }
+        });
+    }
+}
+
+function registerUser(){
+    var phoneNumber  = document.getElementById("registerPhoneNumber").value.trim();
+    var name = document.getElementById("fullName").value.trim();
+    var password = document.getElementById("registerPassword").value.trim();
+
+    if(! phoneNumber.match(phoneNumberPattern)){
+        addValidationError("Invalid phone number");
+        return ;
+    }
+    if(!(name && name.length>0)){
+        addValidationError("Invalid name");
+        return ; 
+    }
+    if(password.length<6){
+        addValidationError("Password must be atleast 6 characters");
+        return ;
+    }
+
+    const userData = {
+        "name" : name,
+        "phone" : phoneNumber,
+        "password" : password
+    }
+
+    const registerResponse = post(url+"register", userData);
+
+    if(registerResponse["status"]==="error"){
+        addValidationError(registerResponse["message"]);
+        return ;
+    }
+
+    clearValidationErrors();
+    var x = document.getElementById("snackbar");
+    x.innerHTML = "Registered Sucessfully ! Login to Continue";
+    x.className = "show";
+    setTimeout(function(){ x.className = x.className.replace("show", ""); }, 3000);
+    showLoginForm();
+}
+
+function addValidationError(message){
+    document.getElementById("loginErrorMessage").textContent = message;
+    document.getElementById("registerErrorMessage").textContent = message;
+}
+
+function clearValidationErrors(){
+    document.getElementById("loginErrorMessage").textContent = "";
+    document.getElementById("registerErrorMessage").textContent = "";
+}
+
+
+async function get(url){
+    const rawResponse = await fetch(url, {
+        method: 'GET',
+        headers: {
+          'Accept': 'application/json',
+          'Content-Type': 'application/json',
+          'token' : localStorage.getItem('token')
+        }
+    });
+    const content = await rawResponse.json();
+    return content;
+}
+
+async function post(url,data){
+    const rawResponse = await fetch(url, {
+      method: 'POST',
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json',
+        'token' : localStorage.getItem('token')
+      },
+      body: JSON.stringify(data)
+    });
+    const content = await rawResponse.json();
+    return content;
+}
---
+++ scripts/stocks.js
@@ -0,0 +1,210 @@
+const url = "http://127.0.0.1:5000/";
+
+var totalStocks = "";
+var currentPage = 1;
+var totalPages = 0;
+
+var timeout = null;
+
+window.onload = ()=>{ 
+    setUserName(); 
+    getStocks();
+    checkLoginStatus();
+};
+
+stocksToSubscribe = []; 
+stocksToUnSubscribe = [];
+
+function checkLoginStatus(){
+    if(localStorage.getItem("userName") === null){
+        localStorage.clear();
+        window.location.href = "index.html";
+    }
+}
+
+function searchBoxInput(){
+    if(timeout != null){
+        clearTimeout(timeout);
+        timeout = null;
+    }
+    timeout = setTimeout(()=>{ getStocks(); },1000);
+}
+
+function setUserName(){
+    userName = localStorage.getItem("userName");
+    console.log(document.getElementById("logoutButton"));
+    document.getElementById("logoutButton").textContent =`Hello, ${userName} !`;
+}
+
+async function getStocks(){
+    const data = {
+        "name" : document.getElementById("searchBox").value,
+        "page" : currentPage
+    }
+    const stocksResponse = await post(url+"getStocksList",data);
+    const stocks = stocksResponse["stockList"];
+    totalStocks = stocksResponse["total_records"];
+    const stocksTable = document.getElementById("stocksTableBody");
+
+    if(totalStocks === 0){
+        document.getElementById("emptyStocksMessage").style.display = "block";
+        document.getElementById("emptyStocksMessage").textContent = "No Stocks found with given name!";
+    }
+    else{
+        document.getElementById("emptyStocksMessage").style.display = "none";
+    }
+
+    stocksTable.innerHTML = "";
+
+    totalPages = Math.ceil(totalStocks/50);
+
+    console.log(totalStocks+" "+totalPages);
+
+    if(totalPages<=1){
+        document.getElementById("pagination").style.display = "none";
+        document.getElementById("paginationMessage").style.display = "none";
+    }
+    else{
+        changePaginationText();
+    }
+
+    let index = 1;
+
+    stocks.forEach(stock => {
+        const row = document.createElement("tr");
+        row.innerHTML = `<td class="text-center">${ index++ }</td>
+        <td class="text-center">
+            <div class="form-check">
+                <input class="form-check-input" type="checkbox" onclick="changeSubscriptionStatus(${stock.id})" id="userStockStatus${stock.id}"">
+            </div>
+        </td>
+        <td class="text-center">${stock.name}</td>
+        <td class="text-center">${stock.symbol}</td>
+        <td class="text-center">${stock.industry}</td>`;
+        stocksTable.appendChild(row);
+        if(stock.subscribed){
+            document.getElementById(`userStockStatus${stock.id}`).checked = true;
+        }
+    });
+}
+
+async function changeSubscriptionStatus(stockId){
+
+    document.getElementById("saveChangesButton").disabled = false;
+
+    const element = document.getElementById(`userStockStatus${stockId}`);
+    if(element.checked){
+        const subscribeIndex = stocksToSubscribe.indexOf(stockId);
+        const unsubscribeIndex = stocksToUnSubscribe.indexOf(stockId);
+
+        if(unsubscribeIndex >=0){
+            stocksToUnSubscribe.splice(unsubscribeIndex, 1);
+        }
+        if(subscribeIndex == -1){
+            stocksToSubscribe.push(stockId); 
+        } 
+    }
+    else{
+        const subscribeIndex = stocksToSubscribe.indexOf(stockId);
+        const unsubscribeIndex = stocksToUnSubscribe.indexOf(stockId);
+        if(subscribeIndex >=0){
+            stocksToSubscribe.splice(subscribeIndex,1);
+        }
+        if(unsubscribeIndex === -1){
+            stocksToUnSubscribe.push(stockId); 
+        }
+    }
+}
+
+async function saveSubscriptions(){
+    const data = {
+        "stocks_to_subscribe" : stocksToSubscribe,
+        "stocks_to_unsubscribe" : stocksToUnSubscribe
+    }
+    const saveSubscriptionsResponse = await post(url+"subscribeStocks",data);
+    var x = document.getElementById("snackbar");
+    if(saveSubscriptionsResponse["status"]=="success"){
+        x.innerHTML = "Changes Updated Successfully !";
+    }
+    else{
+        x.innerHTML = "There was some error ! Please Try Again"
+    }
+    
+    stocksToUnSubscribe = [];
+    stocksToUnSubscribe =[]; 
+
+    document.getElementById("saveChangesButton").disabled = true;
+
+    x.className = "show";
+    setTimeout(function(){ x.className = x.className.replace("show", ""); }, 3000);
+}
+
+
+
+// PAGINATION FUNCTIONS
+
+function nextPage(){
+    if(currentPage===totalPages){
+        return ;
+    }
+    document.getElementById("firstBoxInner").textContent = ++currentPage;
+    if(currentPage===totalPages){
+        document.getElementById("next_page").classList.add("disabled");
+    } 
+    document.getElementById("previous_page").classList.remove("disabled");
+    getStocks();
+    changePaginationText();
+}
+
+function previousPage(){
+    if(currentPage === 1){
+        return ;
+    }
+    document.getElementById("firstBoxInner").textContent = --currentPage;
+    if(currentPage==1){
+        document.getElementById("previous_page").classList.add("disabled");
+    }
+    document.getElementById("next_page").classList.remove("disabled");
+    getStocks();
+    changePaginationText();
+}
+
+function changePaginationText(){
+    document.getElementById("paginationMessage").textContent = `Page ${currentPage} of ${totalPages}`
+}
+
+//Logout function
+
+function logoutUser(){
+    localStorage.clear();
+    window.location.href = "index.html";
+}
+
+// HTTP REQUESTS
+
+async function get(url){
+    const rawResponse = await fetch(url, {
+        method: 'GET',
+        headers: {
+          'Accept': 'application/json',
+          'Content-Type': 'application/json',
+          'token' : localStorage.getItem('token')
+        }
+    });
+    const content = await rawResponse.json();
+    return content;
+}
+
+async function post(url,data){
+    const rawResponse = await fetch(url, {
+      method: 'POST',
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json',
+        'token' : localStorage.getItem('token')
+      },
+      body: JSON.stringify(data)
+    });
+    const content = await rawResponse.json();
+    return content;
+}
